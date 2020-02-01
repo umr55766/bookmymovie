f11eb0b574a3911b7221bca9a5692d59b6025fe9
Umair <umr55766@gmail.com>
2020-02-01 14:58:37 +0530

    Fix model relationship

--- bookmymovie/theaters/models.py
+++ bookmymovie/theaters/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
+class Theater(TimeStampedModel):
+    name = models.CharField(max_length=1024)
+
+
 class Seat(TimeStampedModel):
     row = models.CharField(max_length=2)
     number = models.IntegerField()
     is_aisle = models.BooleanField(default=False)
-
-
-class Theater(TimeStampedModel):
-    name = models.CharField(max_length=1024)
-    seats = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
