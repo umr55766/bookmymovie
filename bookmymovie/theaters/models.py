6d909d0e279d9f1888e75058bf6b437c60696dee
Umair <umr55766@gmail.com>
2020-02-01 14:45:34 +0530

    Make Theater & Seat model TimeStampedModels

--- bookmymovie/theaters/models.py
+++ bookmymovie/theaters/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 
-class Seat(models.Model):
+class Seat(TimeStampedModel):
     row = models.CharField(max_length=2)
     number = models.IntegerField()
     is_aisle = models.BooleanField(default=False)
 
 
-class Theater(models.Model):
+class Theater(TimeStampedModel):
     name = models.CharField(max_length=1024)
     seats = models.ForeignKey(Seat, on_delete=models.CASCADE)
